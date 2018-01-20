a5f2c267522c97e845f07408f788df5e056f16ca
vswtruong <vswtruong@gmail.com>
2018-01-20 14:18:12 -0500

    Hello Van

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const trivia = document.getElementById("trivia");
+const videoButton = document.getElementById("videoButton");
+const seagalPic = document.getElementById('seagalPic');
+const seagalVideo = document.getElementById("seagalVideo")
 
+triviaFunc = () => {
+  alert("Please enter the letter for the corresponding values")
+  const value1 = prompt("What was Steven Seagal's first movie? a: Above the Law, b. Under the Siege, c. The Glimmer Man");
+
+if(value1 === "a"){
+  alert("You got it right!")
+}  else {
+  alert("Incorrect, please try again.")
+}
+}
+
+
+trivia.addEventListener("click", function(){
+  triviaFunc();
+})
+
+videoButton.addEventListener("click", function(){
+  seagalPic.classList.toggle("displayClass");
+seagalVideo.classList.toggle("displayClass");
+});
